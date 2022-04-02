27a077f8bb50ae469f5b7680757aadf59d0688a1
Rafat Haque <rafathaque1997@gmail.com>
2022-04-02 16:24:53 +0600

    update exception handler code

---
+++ packages/exceptionHandler/src/JsonSchemaRequestValidationError.ts
@@ -0,0 +1,18 @@
+import { CustomError } from "./CustomError";
+import { ErrorObject } from "ajv";
+
+export class JsonSchemaRequestValidationError extends CustomError {
+  statusCode = 400;
+
+  constructor(
+    public message: string,
+    public errors?: Object[] | ErrorObject[] | null | undefined
+  ) {
+    super(message);
+    Object.setPrototypeOf(this, JsonSchemaRequestValidationError.prototype);
+  }
+
+  serializeErrors() {
+    return { message: this.message, details: this.errors };
+  }
+}
